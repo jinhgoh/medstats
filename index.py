048b1848f28869bfee6ba27475f848b4c5884292
Jinhyong Goh <68317603+jinhgoh@users.noreply.github.com>
2024-06-04 16:44:22 +0900

    Create index.py

---
+++ index.py
@@ -0,0 +1,36 @@
+import streamlit as st
+import pandas as pd
+from scipy.stats import chi2_contingency
+
+# Title of the application
+st.title("카이제곱분포 분석기 for Dr.Cho, DVM, Ph.D.")
+
+# Upload Excel file
+uploaded_file = st.file_uploader("엑셀파일업로드", type=["xlsx"])
+
+if uploaded_file:
+    # Read the Excel file
+    df = pd.read_excel(uploaded_file)
+    
+    # Show the dataframe
+    st.write("입력된 데이터:", df)
+    
+    # Select dependent and independent variables
+    dependent_var = st.selectbox("변수1을 선택하세요", df.columns)
+    independent_var = st.selectbox("변수2를 선택하세요", df.columns)
+    
+    if st.button("Run Chi-square Analysis"):
+        # Create a contingency table
+        contingency_table = pd.crosstab(df[dependent_var], df[independent_var])
+        #st.write(contingency_table)
+        # Perform Chi-square test
+        chi2, p, dof, expected = chi2_contingency(contingency_table, correction=False)
+        
+        # Display the results
+        st.write("Chi-square test results:")
+        st.write(f"Chi-square statistic: {chi2}")
+        st.write(f"P-value: {p}")
+        st.write(f"Degrees of freedom: {dof}")
+        st.write("Expected frequencies:")
+        st.write(expected)
+
